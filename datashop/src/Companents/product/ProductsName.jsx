9015a63a1f1e4c16f568fb5aa229386e3b199df3
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2023-07-15 17:44:46 +0500

    add

---
+++ datashop/src/Companents/product/ProductsName.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProductsName() {
+  return (
+    <div className='ProductsName'>
+        <p className="pName"><i className="fa-solid fa-laptop" style={{color:"#000000"}}></i> Ноутбуки</p>
+        <p className="productall">Смотреть еще <i className="fa-solid fa-arrow-right" style={{color:"#000000"}}></i></p>
+    </div>
+  )
+}
--- datashop/src/Pages/Products.jsx
+++ datashop/src/Pages/Products.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import Product from '../Companents/product/Product'
+import ProductsName from '../Companents/product/ProductsName'
 
 export default function Products() {
   return (
     <div className='Products'>
-        <Product/>
+      <ProductsName />
+      <div className="allProducts">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   )
 }
